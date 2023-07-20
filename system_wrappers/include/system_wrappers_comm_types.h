2edca4388c463dbf6c77ef7e255754d6770e14d0
liweijie <liweijie@offcn.com>
2023-07-20 18:41:37 +0800

    add all file

---
+++ system_wrappers/include/system_wrappers_comm_types.h
@@ -0,0 +1,9 @@
+#ifndef _SYSTEM_WRAPPERS_COMMON_TYPES_H_
+#define _SYSTEM_WRAPPERS_COMMON_TYPES_H_
+
+#include <stddef.h>
+#include <string.h>
+#include <string>
+#include <vector>
+
+#endif  // COMMON_TYPES_H_
